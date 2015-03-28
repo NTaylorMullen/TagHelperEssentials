5e7e180717d32a06138925afa8d33960b12c9f9f
N. Taylor Mullen <nimullen@microsoft.com>
2015-03-28 15:17:59 -0700

    Initial source commits.
    - Added sln.
    - Added initial src/samples.

---
+++ samples/TagHelperEssentials.Web/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.Framework.DependencyInjection;
+
+namespace TagHelperEssentials.Web
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc(routes => routes.MapRoute("default", "{Controller=Home}/{Action=Index}"));
+        }
+    }
+}
---
+++ src/TagHelperEssentials/VisibleAttributeTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Razor.Runtime.TagHelpers;
+
+namespace TagHelperEssentials
+{
+    [TargetElement(Attributes = VisiblePropertyName)]
+    public class VisibleAttributeTagHelper : TagHelper
+    {
+        private const string VisiblePropertyName = "asp-visible";
+
+        [HtmlAttributeName(VisiblePropertyName)]
+        public bool Visible { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (!Visible)
+            {
+                output.SuppressOutput();
+            }
+        }
+    }
+}
